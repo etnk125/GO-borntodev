c70bddc297440bd623fe2194ef215078692d7fa1
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-02-27 23:05:12 +0700

    db

--- webservice/db/db.go
+++ webservice/db/db.go
@@ -3,3 +3,7 @@ package main
 func main() {
 
 }
+
+//go mod init github.com/etnk125/go-borntodev/webservice/db
+//go get -u github.com/go-sql-driver/mysql
+// https://academy.borntodev.com/courses/1624977/lectures/37905083
